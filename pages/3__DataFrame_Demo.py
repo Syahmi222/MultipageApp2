238c1c79616d580487f74f228665a12d57a07e4b
Syahmi222 <m152510007@student.utem.edu.my>
2026-05-24 22:11:15 +0800

    Create 3__DataFrame_Demo.py

---
+++ pages/3__DataFrame_Demo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("DataFrame Demo")
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 5),
+    columns=('col %d' % i for i in range(5))
+)
+
+st.dataframe(dataframe)
