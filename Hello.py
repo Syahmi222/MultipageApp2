aa9643ad16502b8464ab983852bda1184f058ec3
Syahmi222 <m152510007@student.utem.edu.my>
2026-05-24 22:11:04 +0800

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+
+    ### Want to learn more?
+    - Check out https://streamlit.io
+    - Jump into our documentation
+    """
+)
